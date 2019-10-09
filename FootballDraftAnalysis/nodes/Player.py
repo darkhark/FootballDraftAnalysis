daecbc39fcefb612acf7e18d6aace2f81dd012a7
darkhark <joshua.harkness32@gmail.com>
2019-10-09 17:54:17 -0400

    Added comments so others can see create the other two classes using this as a reference.

--- FootballDraftAnalysis/nodes/Player.py
+++ FootballDraftAnalysis/nodes/Player.py
@@ -1,23 +1,32 @@
 from nodes import Node
 
 
+# Node is in parenthesis so that it can inherit the abstract class and all of its methods.
 class Player(Node):
+    # This is a static variable. The same value exists for every instance of this class.
     uniqueIDNumber = 0
 
-    def __init__(self, name, position, college, roundSelected, yearDrafted, nflTeam, allPros, proBowls):
+    # This is the constructor for the class. The constructor is called anytime we need this object.
+    def __init__(self, name, position, collegeID, roundSelected, yearDrafted, nflTeamID, allPros, proBowls):
+        # This is how python creates local variables that can used in other method of the class.
         self.name = name
         self.position = position
-        self.college = college
+        self.collegeID = collegeID
         self.roundSelected = roundSelected
         self.yearDrafted = yearDrafted
-        self.nflTeam = nflTeam
+        self.nflTeamID = nflTeamID
         self.allPros = allPros
         self.proBowls = proBowls
         self.uniqueID = "PL" + str(self.uniqueIDNumber)
-        self.uniqueIDNumber = self.uniqueIDNumber + 1
+        # This increases the static variable by 1. Doing this will make each ID different, even though it looks like
+        # every object will have an ID of PL0.
+        Player.uniqueIDNumber += 1
 
+    # This method overrides the same method in Node. Because the method is abstract in Node.py, it is required here.
     def getName(self):
         return self.name
 
+    # This method overrides the same method in Node. Because the method is abstract in Node.py, it is required here.
     def getUniqueID(self):
         return self.uniqueID
+
